0fa9bb54fcec75643c16e84c0a6f674d8901a39f
usernow <usernow@gmail.com>
2022-11-17 16:10:03 -0500

    feat: ensure SignUpController return 500 if EmailValidator throws

--- src/presentation/controllers/singup.ts
+++ src/presentation/controllers/singup.ts
@@ -1,9 +1,10 @@
 import { MissingParamError } from "../errors/MissinParamError";
 import { HttpRequest, HttpResponse } from "../protocols/http";
-import { badRequest } from "../helpers/badRequest";
+import { badRequest, internalError } from "../helpers/badRequest";
 import { Controller } from "../protocols/controller";
 import { EmailValidator } from "../protocols/emailValidator";
 import { InvalidParamError } from "../errors/InvalidParamError";
+import { ServerError } from "../errors/ServerError";
 
 export class SignUpController implements Controller {
     private readonly emailValidator: EmailValidator;
@@ -13,15 +14,19 @@ export class SignUpController implements Controller {
     }
 
     handle (httpRequest: HttpRequest): HttpResponse {
-        const requiredFields = ["name", "email", "password", "passwordConfirmation"];
-        for (const field of requiredFields) {
-            if (!httpRequest.body[field]) {
-                return badRequest(new MissingParamError(field));
+        try {
+            const requiredFields = ["name", "email", "password", "passwordConfirmation"];
+            for (const field of requiredFields) {
+                if (!httpRequest.body[field]) {
+                    return badRequest(new MissingParamError(field));
+                }
             }
+            const { email } = httpRequest.body;
+            const isValid = this.emailValidator.isValid(email);
+            if (!isValid) return badRequest(new InvalidParamError(email));
+            return { statusCode: 200, body: {} };
+        } catch (error) {
+            return internalError(new ServerError());
         }
-        const { email } = httpRequest.body;
-        const isValid = this.emailValidator.isValid(email);
-        if (!isValid) return badRequest(new InvalidParamError(email));
-        return { statusCode: 200, body: {} };
     }
 }
