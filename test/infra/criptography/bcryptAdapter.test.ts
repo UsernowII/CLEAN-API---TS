78d6ac0979ca091c62bedb205aaadf54ad6350fd
usernow <usernow@gmail.com>
2022-12-02 11:24:38 -0500

    refactor: move sut Bcrypt creation to a factory method

--- test/infra/criptography/bcryptAdapter.test.ts
+++ test/infra/criptography/bcryptAdapter.test.ts
@@ -1,24 +1,27 @@
 import bcrypt from "bcrypt";
 import { BcryptAdapter } from "../../../src/infra/criptography/bcryptAdapter";
 
-jest.mock("bcrypt", ()=> ({
-  hash (): Promise<string> {
-    return new Promise( resolve => resolve("hash_value"));
+jest.mock("bcrypt", () => ({
+  hash(): Promise<string> {
+    return new Promise(resolve => resolve("hash_value"));
   }
 }));
 
+const salt = 5;
+const makeSut = (): BcryptAdapter => {
+  return new BcryptAdapter(salt);
+}
+
 describe('Bcrypt Adapter', () => {
   test('Should call bcrypt with correct values', async () => {
-      const salt = 5;
-      const sut = new BcryptAdapter(salt);
-      const hashSpy = jest.spyOn(bcrypt, "hash");
-      await sut.encrypt("any_value");
-      expect(hashSpy).toHaveBeenCalledWith("any_value", salt);
+    const sut = makeSut();
+    const hashSpy = jest.spyOn(bcrypt, "hash");
+    await sut.encrypt("any_value");
+    expect(hashSpy).toHaveBeenCalledWith("any_value", salt);
   });
   test('Should return a hash on success', async () => {
-      const salt = 5;
-      const sut = new BcryptAdapter(salt);
-      const hash = await sut.encrypt("any_value");
-      expect(hash).toBe("hash_value");
+    const sut = makeSut();
+    const hash = await sut.encrypt("any_value");
+    expect(hash).toBe("hash_value");
   });
 });
