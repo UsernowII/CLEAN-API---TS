ff090d2a2a0308a492a30b058361246ec41b7519
usernow <usernow@gmail.com>
2022-12-06 12:41:10 -0500

    feat: ensure SignUp route works and return success

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import setupMiddaleware from "./middlewares";
+import setupRoutes from "./routes";
 
 const app = express();
 setupMiddaleware(app);
+setupRoutes(app);
 
 export default app;
