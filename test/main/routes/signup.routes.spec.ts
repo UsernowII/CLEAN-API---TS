83f77fcfb3be19019cda9d45880725dbfe7af76a
usernow <usernow@gmail.com>
2022-12-09 18:24:55 -0500

    refactor: connect to mongodb before integration tests

--- test/main/routes/signup.routes.spec.ts
+++ test/main/routes/signup.routes.spec.ts
@@ -1,7 +1,21 @@
 import request from "supertest";
+import { MongoHelper } from "../../../src/infra/db/mongodb/mongo-helpers";
 import app from "../../../src/main/config/app";
 
 describe('signup Routes', () => {
+
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  afterEach(async () => {
+    await MongoHelper.getCollection("accounts").deleteMany({});
+  })
+
   test('Should return an account on success', async () => {
     await request(app)
       .post("/api/signup")
