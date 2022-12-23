14c18a3cc344cd614ba7a95f2cdd0963559ce1fe
usernow <usernow@gmail.com>
2022-12-23 18:06:39 -0500

    feat: ensure LoginController calls EmailValidator with correct email

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,8 +1,15 @@
 import { Controller, HttpRequest, HttpResponse } from "../../protocols";
 import { badRequest } from "../../helpers/http-helper";
 import { MissingParamError } from "../../errors";
+import { EmailValidator } from "../../protocols/emailValidator";
 
 export class LoginController implements Controller {
+    private readonly emailValidator: EmailValidator;
+
+    constructor (emailValidator: EmailValidator) {
+        this.emailValidator = emailValidator;
+    }
+
     async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
         if (!httpRequest.body.email) {
             return badRequest(new MissingParamError("email"));
@@ -10,6 +17,8 @@ export class LoginController implements Controller {
         if (!httpRequest.body.password) {
             return badRequest(new MissingParamError("password"));
         }
+        const { email } = httpRequest.body;
+        this.emailValidator.isValid(email);
         return null;
     };
 }
