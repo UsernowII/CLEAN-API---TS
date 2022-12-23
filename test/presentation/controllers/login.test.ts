fc8394c308dfe32f3260c09ca8bf030be3e74d41
usernow <usernow@gmail.com>
2022-12-23 17:36:34 -0500

    refactor: move sut creation to a factory method

--- test/presentation/controllers/login.test.ts
+++ test/presentation/controllers/login.test.ts
@@ -3,9 +3,20 @@ import { HttpRequest } from '../../../src/presentation/protocols/http';
 import { badRequest } from "../../../src/presentation/helpers/http-helper";
 import { MissingParamError } from '../../../src/presentation/errors/MissinParamError';
 
+interface SutTypes {
+	sut: LoginController,
+}
+
+const makesut = (): SutTypes => {
+	const sut = new LoginController();
+	return {
+		sut,
+	}
+};
+
 describe('Login Controller', () => {
   test('Should return 400 if no email is provided', async () => {
-    const sut = new LoginController();
+    const { sut } = makesut();
 		const httpRequest: HttpRequest = {
 			body: { 
 				password: "any_password"
@@ -15,13 +26,23 @@ describe('Login Controller', () => {
 		expect(httpResponse).toEqual(badRequest(new MissingParamError("email")))
   });
   test('Should return 400 if no password is provided', async () => {
-    const sut = new LoginController();
+    const { sut } = makesut();
 		const httpRequest: HttpRequest = {
 			body: { 
 				email: "any_email@email.com"
 			}
 		}
 		const httpResponse = await sut.handle(httpRequest);
-		expect(httpResponse).toEqual(badRequest(new MissingParamError("pasword")))
+		expect(httpResponse).toEqual(badRequest(new MissingParamError("password")))
+  });
+	test('Should return 400 if no password is provided', async () => {
+    const { sut } = makesut();
+		const httpRequest: HttpRequest = {
+			body: { 
+				email: "any_email@email.com"
+			}
+		}
+		const httpResponse = await sut.handle(httpRequest);
+		expect(httpResponse).toEqual(badRequest(new MissingParamError("password")))
   });
 });
