7ad089edcb994e7437c01cad07e5dd3f606025ff
usernow <usernow@gmail.com>
2023-01-13 14:14:31 -0500

    feat: ensure DbAuthentication calls LoadAccountRepository with correct email

---
+++ src/data/usecases/auth/db-authentication.ts
@@ -0,0 +1,15 @@
+import { Authentication, AuthenticationModel } from "../../../domain/usecases/authentication";
+import { LoadAccountByEmailRepository } from "../../protocols/loadAccountByEmailRepository";
+
+export class DbAuthentication implements Authentication {
+    private readonly loadAccountByEmailRepo: LoadAccountByEmailRepository;
+
+    constructor (loadAccountByEmailRepo: LoadAccountByEmailRepository) {
+        this.loadAccountByEmailRepo = loadAccountByEmailRepo;
+    }
+
+    async auth (auth: AuthenticationModel): Promise<string> {
+        await this.loadAccountByEmailRepo.load(auth.email);
+        return null;
+    };
+}
