bb03cd2416d579d454f5f53b1d0cd62d23d4ac93
usernow <usernow@gmail.com>
2023-01-13 17:01:35 -0500

    feat: ensure DbAuthentication calls UpdateAccessTokenRepository with correct values

--- src/data/usecases/auth/db-authentication.ts
+++ src/data/usecases/auth/db-authentication.ts
@@ -2,20 +2,24 @@ import { Authentication, AuthenticationModel } from "../../../domain/usecases/au
 import { HashComparer } from "../../protocols/criptography/hashComparer";
 import { LoadAccountByEmailRepository } from "../../protocols/db/loadAccountByEmailRepository";
 import { TokenGenerator } from "../../protocols/criptography/tokenGenerator";
+import { UpdateAccessTokenRepository } from "../../protocols/db/updateAccessTokenRepository";
 
 export class DbAuthentication implements Authentication {
     private readonly loadAccountByEmailRepo: LoadAccountByEmailRepository;
     private readonly hashComparer: HashComparer;
     private readonly tokenGenerator: TokenGenerator;
+    private readonly updateAccessTokenRepository: UpdateAccessTokenRepository;
 
     constructor (
         loadAccountByEmailRepo: LoadAccountByEmailRepository,
         hashComparer: HashComparer,
-        tokenGenerator: TokenGenerator
+        tokenGenerator: TokenGenerator,
+        updateAccessTokenRepository: UpdateAccessTokenRepository
     ) {
         this.loadAccountByEmailRepo = loadAccountByEmailRepo;
         this.hashComparer = hashComparer;
         this.tokenGenerator = tokenGenerator;
+        this.updateAccessTokenRepository = updateAccessTokenRepository;
     }
 
     async auth (auth: AuthenticationModel): Promise<string> {
@@ -23,6 +27,8 @@ export class DbAuthentication implements Authentication {
         if (!account) return null;
         const isValid = await this.hashComparer.compare(auth.password, account.password);
         if (!isValid) return null;
-        return await this.tokenGenerator.generate(account.id);
+        const accessToken = await this.tokenGenerator.generate(account.id);
+        await this.updateAccessTokenRepository.update(account.id, accessToken);
+        return accessToken;
     };
 }
