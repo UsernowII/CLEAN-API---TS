516eec406d40e9a29ed0a9556dc6e8d61da95841
usernow <usernow@gmail.com>
2023-01-15 17:06:45 -0500

    test: ensure AccountMongoRepository return an account when loadByEmail success

--- test/infra/db/mongodb/account-repository/account.test.ts
+++ test/infra/db/mongodb/account-repository/account.test.ts
@@ -1,5 +1,8 @@
+import { Collection } from "mongodb";
 import { AccountMongoRepository } from "../../../../../src/infra/db/mongodb/account-repository/accountMongoRepository";
 import { MongoHelper } from "../../../../../src/infra/db/mongodb/helpers/mongo-helper";
+
+let accountCollection: Collection;
 
 describe('Account Mongo Repository', () => {
 
@@ -11,15 +14,16 @@ describe('Account Mongo Repository', () => {
     await MongoHelper.disconnect();
   });
 
-  afterEach(async () => {
-    await (await MongoHelper.getCollection("accounts")).deleteMany({});
+  beforeEach(async () => {
+    accountCollection = await MongoHelper.getCollection("accounts");
+    await accountCollection.deleteMany({});
   })
 
   const makeSut = (): AccountMongoRepository => {
     return new AccountMongoRepository();
   }
 
-  test('Should return an account on success', async () => {
+  test('Should return an account on add success', async () => {
     const sut = makeSut();
     const account = await sut.add({
       name: "any_name",
@@ -32,4 +36,20 @@ describe('Account Mongo Repository', () => {
     expect(account.email).toBe("any_email@email.com");
     expect(account.password).toBe("any_password");
   });
+
+  test('Should return an account when loadByEmail success', async () => {
+    const sut = makeSut();
+    await accountCollection.insertOne({
+      name: "any_name",
+      email: "any_email@email.com",
+      password: "any_password",
+    })
+    const account = await sut.loadByEmail("any_email@email.com");
+    expect(account).toBeTruthy();
+    expect(account.id).toBeTruthy();
+    expect(account.name).toBe("any_name");
+    expect(account.email).toBe("any_email@email.com");
+    expect(account.password).toBe("any_password");
+  });
+
 });
