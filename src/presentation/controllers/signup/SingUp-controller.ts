892a4e02c86ca6aa444a06af8e0b03015c505b5d
usernow <usernow@gmail.com>
2023-03-09 15:02:51 -0500

    feat: ensure SignUpController calls Authtentication with correct values

--- src/presentation/controllers/signup/SingUp-controller.ts
+++ src/presentation/controllers/signup/SingUp-controller.ts
@@ -1,10 +1,11 @@
-import { Controller, HttpRequest, HttpResponse, AddAccount, Validation } from "./signup-protocols";
+import { Controller, HttpRequest, HttpResponse, AddAccount, Validation, Authentication } from "./signup-protocols";
 import { badRequest, serverError, createdOk } from "../../helpers/http/http-helper";
 
 export class SignUpController implements Controller {
     constructor (
         private readonly addAccount: AddAccount,
-        private readonly validation: Validation
+        private readonly validation: Validation,
+        private readonly authentication: Authentication
     ) {}
 
     async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
@@ -20,6 +21,7 @@ export class SignUpController implements Controller {
                 email,
                 password
             });
+            await this.authentication.auth({ email, password });
             return createdOk(account);
         } catch (error) {
             return serverError(error);
--- src/presentation/controllers/signup/signup-protocols.ts
+++ src/presentation/controllers/signup/signup-protocols.ts
@@ -2,3 +2,4 @@ export * from "../../protocols";
 export * from "../../../domain/usecases/addAccount";
 export * from "../../../domain/models/account";
 export * from "../../protocols/validation";
+export * from "../login/login-protocols";
