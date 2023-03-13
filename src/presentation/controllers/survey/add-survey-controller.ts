d2f0959e2619103a8527c8ca708be55b3d9d9915
usernow <usernow@gmail.com>
2023-03-12 19:21:25 -0500

    feat: ensure AddSurveyController returns 500 if AddSurvey throws

--- src/presentation/controllers/survey/add-survey-controller.ts
+++ src/presentation/controllers/survey/add-survey-controller.ts
@@ -1,4 +1,4 @@
-import { badRequest } from "../../helpers/http/http-helper";
+import { badRequest, serverError } from "../../helpers/http/http-helper";
 import { AddSurvey, Controller, HttpRequest, HttpResponse, Validation } from "./add-survey-protocols";
 
 export class AddSurveyController implements Controller {
@@ -10,7 +10,11 @@ export class AddSurveyController implements Controller {
     async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
         const error = this.validation.validate(httpRequest.body);
         if (error) return badRequest(error);
-        await this.addSurvey.add(httpRequest.body);
-        return null;
+        try {
+            await this.addSurvey.add(httpRequest.body);
+            return null;
+        } catch (error) {
+            return serverError(error);
+        }
     };
 }
