9ae274ccdefb2edcdff20507eb0d4a5d178490b7
usernow <usernow@gmail.com>
2023-03-13 16:03:40 -0500

    feat: ensure AuthMiddleware return 200 if LoadAccountByToken success

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,5 +1,5 @@
 import { HttpRequest, HttpResponse, Middleware } from "../protocols";
-import { forbidden } from "../helpers/http/http-helper";
+import { forbidden, ok } from "../helpers/http/http-helper";
 import { AccessDeniedError } from "../errors";
 import { LoadAccountByToken } from "../../domain/usecases/loadAccountByToken";
 
@@ -9,7 +9,8 @@ export class AuthMiddleware implements Middleware {
     async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
         const accessToken = httpRequest.headers?.["x-access-token"];
         if (accessToken) {
-            await this.loadAccountBytoken.load(accessToken);
+            const account = await this.loadAccountBytoken.load(accessToken);
+            if (account) return ok({ accountId: account.id });
         }
         return forbidden(new AccessDeniedError());
     }
