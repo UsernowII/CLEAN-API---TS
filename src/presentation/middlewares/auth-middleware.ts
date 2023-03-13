7781779faa4b5a28ab4e22a6f5ab5c0b9cbe375e
usernow <usernow@gmail.com>
2023-03-13 15:29:41 -0500

    feat: ensure AuthMiddleware calls LoadAccountByToken with a token

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,9 +1,16 @@
 import { HttpRequest, HttpResponse, Middleware } from "../protocols";
 import { forbidden } from "../helpers/http/http-helper";
 import { AccessDeniedError } from "../errors";
+import { LoadAccountByToken } from "../../domain/usecases/loadAccountByToken";
 
 export class AuthMiddleware implements Middleware {
+    constructor (private readonly loadAccountBytoken: LoadAccountByToken) {}
+
     async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-        return await Promise.resolve(forbidden(new AccessDeniedError()));
+        const accessToken = httpRequest.headers?.["x-access-token"];
+        if (accessToken) {
+            await this.loadAccountBytoken.load(accessToken);
+        }
+        return forbidden(new AccessDeniedError());
     }
 }
