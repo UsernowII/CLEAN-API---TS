1f6f78154348d28231f2a6fdf7a0e5394cca38e5
usernow <usernow@gmail.com>
2023-03-17 20:02:11 -0500

    feat: ensure DbLoadAccountToken calls repo with correctt values

--- src/data/usecases/load-account-token/db-loadAccountToken.ts
+++ src/data/usecases/load-account-token/db-loadAccountToken.ts
@@ -1,12 +1,19 @@
 import { LoadAccountByToken } from "../../../domain/usecases/loadAccountByToken";
 import { Decrypter } from "../../protocols/criptography/decrypter";
+import { LoadAccountByTokenRepository } from "../../protocols/db/account/loadAccountByTokenRepository";
 import { AccountModel } from "../add-account/db-add-account-protocols";
 
 export class DbLoadAccountToken implements LoadAccountByToken {
-    constructor (private readonly decrypter: Decrypter) {}
+    constructor (
+      private readonly decrypter: Decrypter,
+      private readonly loadByTokenRepo: LoadAccountByTokenRepository
+    ) {}
 
     async load (accessToken: string, role?: string): Promise<AccountModel> {
-        await this.decrypter.decrypt(accessToken);
+        const token = await this.decrypter.decrypt(accessToken);
+        if (accessToken) {
+            await this.loadByTokenRepo.loadByToken(token, role);
+        }
         return null;
     }
 }
