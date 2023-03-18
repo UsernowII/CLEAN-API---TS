c5dab98d7e54560d6bd08d798404b693cdb81975
usernow <usernow@gmail.com>
2023-03-17 19:36:45 -0500

    test: ensure DbLoadAacount return null if Decrypter return null

--- test/data/usecases/load-account-token/db-loadAccountToken.test.ts
+++ test/data/usecases/load-account-token/db-loadAccountToken.test.ts
@@ -25,7 +25,13 @@ describe('Db LoadAccountToken UseCase', () => {
   test('Should call Decrypter with correct values', async () => {
     const { sut, decreypterStub } = makeSut();
     const decryptSpy = jest.spyOn(decreypterStub, "decrypt");
-    await sut.load('any_token');
+    await sut.load('any_token', 'any_role');
     expect(decryptSpy).toHaveBeenCalledWith("any_token");
+  });
+  test('Should return null if Decrypter return null', async () => {
+    const { sut, decreypterStub } = makeSut();
+    jest.spyOn(decreypterStub, "decrypt").mockResolvedValueOnce(null);
+    const account = await sut.load('any_token', 'any_role');
+    expect(account).toBeNull();
   });
 });
